a1b543686fbe709cd94bf483d887771aeae7bdde
Manuele Trimarchi <info@trimarchimanuele.it>
2021-03-02 14:41:44 +0100

    Full pending tx data in subscription

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -38,12 +38,13 @@ import (
 // filter is a helper struct that holds meta information over the filter type
 // and associated subscription in the event system.
 type filter struct {
-	typ      Type
-	deadline *time.Timer // filter is inactiv when deadline triggers
-	hashes   []common.Hash
-	crit     FilterCriteria
-	logs     []*types.Log
-	s        *Subscription // associated subscription in event system
+	typ          Type
+	deadline     *time.Timer // filter is inactiv when deadline triggers
+	hashes       []common.Hash
+	transactions []*types.TxDetails
+	crit         FilterCriteria
+	logs         []*types.Log
+	s            *Subscription // associated subscription in event system
 }
 
 // PublicFilterAPI offers support to create and manage filters. This will allow external clients to retrieve various
@@ -142,6 +143,39 @@ func (api *PublicFilterAPI) NewPendingTransactionFilter() rpc.ID {
 	return pendingTxSub.ID
 }
 
+// NewPendingTransactionFullFilter creates a filter that fetches pending transaction hashes
+// as transactions enter the pending state and includes the tx data
+func (api *PublicFilterAPI) NewPendingTransactionFullFilter() rpc.ID {
+	var (
+		pendingTxs   = make(chan []*types.TxDetails)
+		pendingTxSub = api.events.SubscribePendingFullTxs(pendingTxs)
+	)
+
+	api.filtersMu.Lock()
+	api.filters[pendingTxSub.ID] = &filter{typ: PendingTransactionsFullSubscription, deadline: time.NewTimer(api.timeout), transactions: make([]*types.TxDetails, 0), s: pendingTxSub}
+	api.filtersMu.Unlock()
+
+	go func() {
+		for {
+			select {
+			case ph := <-pendingTxs:
+				api.filtersMu.Lock()
+				if f, found := api.filters[pendingTxSub.ID]; found {
+					f.transactions = append(f.transactions, ph...)
+				}
+				api.filtersMu.Unlock()
+			case <-pendingTxSub.Err():
+				api.filtersMu.Lock()
+				delete(api.filters, pendingTxSub.ID)
+				api.filtersMu.Unlock()
+				return
+			}
+		}
+	}()
+
+	return pendingTxSub.ID
+}
+
 // NewPendingTransactions creates a subscription that is triggered each time a transaction
 // enters the transaction pool and was signed from one of the transactions this nodes manages.
 func (api *PublicFilterAPI) NewPendingTransactions(ctx context.Context) (*rpc.Subscription, error) {
@@ -173,6 +207,41 @@ func (api *PublicFilterAPI) NewPendingTransactions(ctx context.Context) (*rpc.Su
 			}
 		}
 	})
+
+	return rpcSub, nil
+}
+
+// NewPendingFullTransactions creates a subscription that is triggered each time a transaction
+// enters the transaction pool including the tx data
+func (api *PublicFilterAPI) NewPendingFullTransactions(ctx context.Context) (*rpc.Subscription, error) {
+	notifier, supported := rpc.NotifierFromContext(ctx)
+	if !supported {
+		return &rpc.Subscription{}, rpc.ErrNotificationsUnsupported
+	}
+
+	rpcSub := notifier.CreateSubscription()
+
+	go func() {
+		txs := make(chan []*types.TxDetails, 128)
+		pendingTxSub := api.events.SubscribePendingFullTxs(txs)
+
+		for {
+			select {
+			case transactions := <-txs:
+				// To keep the original behaviour, send a single tx hash in one notification.
+				// TODO(rjl493456442) Send a batch of tx hashes in one notification
+				for _, t := range transactions {
+					notifier.Notify(rpcSub.ID, t)
+				}
+			case <-rpcSub.Err():
+				pendingTxSub.Unsubscribe()
+				return
+			case <-notifier.Closed():
+				pendingTxSub.Unsubscribe()
+				return
+			}
+		}
+	}()
 
 	return rpcSub, nil
 }
@@ -433,6 +502,10 @@ func (api *PublicFilterAPI) GetFilterChanges(id rpc.ID) (interface{}, error) {
 		f.deadline.Reset(api.timeout)
 
 		switch f.typ {
+		case PendingTransactionsFullSubscription:
+			transactions := f.transactions
+			f.transactions = nil
+			return returnTransactions(transactions), nil
 		case PendingTransactionsSubscription, BlocksSubscription:
 			hashes := f.hashes
 			f.hashes = nil
@@ -445,6 +518,13 @@ func (api *PublicFilterAPI) GetFilterChanges(id rpc.ID) (interface{}, error) {
 	}
 
 	return []interface{}{}, fmt.Errorf("filter not found")
+}
+
+func returnTransactions(transactions []*types.TxDetails) []*types.TxDetails {
+	if transactions == nil {
+		return []*types.TxDetails{}
+	}
+	return transactions
 }
 
 // returnHashes is a helper that will return an empty hash array case the given hash array is nil,
