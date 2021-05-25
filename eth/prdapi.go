50aa45469f5d74709e8e76a243c07b62d608b8b3
Remo Lemma <remo.lemma@gmail.com>
2021-05-25 11:41:56 +0200

    Add Logs to Predict Output (+ apply method commented)

--- eth/prdapi.go
+++ eth/prdapi.go
@@ -7,13 +7,38 @@ import (
 	"github.com/ethereum/go-ethereum/internal/ethapi"
 )
 
-func (s *PublicEthereumAPI) PredictBlock(ctx context.Context) (interface{}, error) {
-	block, _, err := s.e.Miner().PredictBlock()
+func (s *PublicEthereumAPI) PredictBlock(ctx context.Context) (interface{}, interface{}, error) {
+	block, receipts, err := s.e.Miner().PredictBlock()
 	if block != nil && err == nil {
-		return s.rpcMarshalBlock(ctx, block, true, true)
+		var logs []*types.Log
+		for _, receipt := range receipts {
+			for _, log := range receipt.Logs {
+				log.BlockHash = block.Hash()
+			}
+			logs = append(logs, receipt.Logs...)
+		}
+
+		b, berr := s.rpcMarshalBlock(ctx, block, true, true)
+		return b, logs, berr
 	}
-	return nil, err
+	return nil, nil, err
 }
+
+/*func (s *PublicEthereumAPI) PredictApplyBlock(ctx context.Context) (interface{}, error) {
+	block, _, err := s.e.Miner().PredictBlock()
+	if err != nil {
+		return nil, err
+	}
+
+	// Apply simulating the block came from the "canonical" chain
+	blocks := make([]*types.Block, 1)
+	blocks[0] = block
+	_, berr := s.e.blockchain.InsertChain(blocks)
+	if berr != nil {
+		return nil, berr
+	}
+	return s.rpcMarshalBlock(ctx, block, true, true)
+}*/
 
 func (s *PublicEthereumAPI) rpcMarshalBlock(ctx context.Context, b *types.Block, inclTx bool, fullTx bool) (map[string]interface{}, error) {
 	fields, err := ethapi.RPCMarshalBlock(b, inclTx, fullTx)
