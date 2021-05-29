f27612a0553aef8f1b3028d9623e4daf7e60f115
Remo Lemma <remo.lemma@gmail.com>
2021-05-29 15:12:14 +0200

    Make max delta configurable via api call (avoid restart)

--- miner/miner.go
+++ miner/miner.go
@@ -83,6 +83,10 @@ func New(eth Backend, config *Config, chainConfig *params.ChainConfig, mux *even
 	return miner
 }
 
+func (miner *Miner) MaxDelta(max int) {
+	miner.worker.maxDelta = uint64(max)
+}
+
 // update keeps track of the downloader events. Please be aware that this is a one shot type of update loop.
 // It's entered once and as soon as `Done` or `Failed` has been broadcasted the events are unregistered and
 // the loop is exited. This to prevent a major security vuln where external parties can DOS you with blocks
