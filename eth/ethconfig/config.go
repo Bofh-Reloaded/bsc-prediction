c9d949e9df8be91f58375dc4ce62578af40eca01
Remo Lemma <remo.lemma@gmail.com>
2021-05-29 11:47:18 +0200

    increase delay

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -86,7 +86,7 @@ var Defaults = Config{
 		GasCeil:       8000000,
 		GasPrice:      big.NewInt(params.GWei),
 		Recommit:      3 * time.Second,
-		DelayLeftOver: 100 * time.Millisecond,
+		DelayLeftOver: 150 * time.Millisecond,
 	},
 	TxPool:      core.DefaultTxPoolConfig,
 	RPCGasCap:   25000000,
