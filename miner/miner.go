c4f924aba01dcf8f9f476bbcccadf5b4c7ed6acd
Remo Lemma <remo.lemma@gmail.com>
2021-05-30 15:43:36 +0200

    Add configurable delay

--- miner/miner.go
+++ miner/miner.go
@@ -91,6 +91,14 @@ func (miner *Miner) GetMaxDelta() uint64 {
 	return miner.worker.maxDelta
 }
 
+func (miner *Miner) SetPredictionDelay(max int) {
+	miner.worker.pDelay = time.Duration(max)
+}
+
+func (miner *Miner) GetPredictionDelay() time.Duration {
+	return miner.worker.pDelay
+}
+
 // update keeps track of the downloader events. Please be aware that this is a one shot type of update loop.
 // It's entered once and as soon as `Done` or `Failed` has been broadcasted the events are unregistered and
 // the loop is exited. This to prevent a major security vuln where external parties can DOS you with blocks
