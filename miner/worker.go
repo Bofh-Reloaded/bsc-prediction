b19e871a9446adb95406e42c5051f7abb05a40e5
Remo Lemma <remo.lemma@gmail.com>
2021-06-07 18:24:05 +0200

    Fix SIGSEGV

--- miner/worker.go
+++ miner/worker.go
@@ -564,7 +564,6 @@ func (w *worker) mainLoop() {
 				txset := types.NewTransactionsByPriceAndNonce(w.current.signer, txs)
 				tcount := w.current.tcount
 				w.commitTransactions(txset, coinbase, nil, 0, m)
-				log.Info("Queued Txs Applied","txs", len(txs))
 				// Only update the snapshot if any new transactons were added
 				// to the pending block
 				if tcount != w.current.tcount {
@@ -612,7 +611,6 @@ func (w *worker) mainLoop() {
 				txset := types.NewTransactionsByPriceAndNonce(w.current.signer, txs)
 				tcount := w.current.tcount
 				w.commitTransactions(txset, coinbase, nil, 0, m)
-				log.Info("Pending Txs Applied","txs", len(txs))
 				// Only update the snapshot if any new transactons were added
 				// to the pending block
 				if tcount != w.current.tcount {
